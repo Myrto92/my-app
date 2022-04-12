9b0b7f8a1a8565e46ebdab13f20de634d21f82dc
Myrto92 <98779941+Myrto92@users.noreply.github.com>
2022-04-12 19:35:34 +0300

    removed unused component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Search from "./Search";
 import Me from "./Me";
+
 function App() {
   return (
     <div className="App">
