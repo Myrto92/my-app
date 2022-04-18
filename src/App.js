bc0b4dc0e51ac36a172079110a6a6ec0603ebba9
Myrto92 <98779941+Myrto92@users.noreply.github.com>
2022-04-18 18:47:14 +0300

    change the icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Search from "./Search";
 import Me from "./Me";
+import WeatherIcon from "./WeatherIcon";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div className="row">
           <div className="col-4">
             <Search />
+            <WeatherIcon code={props.data.icon} />
           </div>
         </div>
         <div className="row"></div>
